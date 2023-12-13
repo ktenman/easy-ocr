7eeb864f3aa01d886377b82532b3a977d6003b98
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-12-13 15:18:20 +0200

    Optimize logging

--- app-redis.py
+++ app-redis.py
@@ -4,11 +4,15 @@ import easyocr
 import logging
 import numpy as np
 import redis
-from pydantic import BaseModel, ValidationError
+import threading
 import time
+from pydantic import BaseModel, ValidationError
 
 # Initialize the OCR reader
 reader = easyocr.Reader(['en'], gpu=True)  # Set 'gpu' to False if you're not using a GPU
+
+# Thread-local storage for UUID
+thread_local_storage = threading.local()
 
 class ImageUploadRequest(BaseModel):
     uuid: str
@@ -29,10 +33,10 @@ def perform_ocr(preprocessed_img):
     logging.debug("OCR performed on the image.")
     return decoded_text.strip() if decoded_text else "No text found"
 
-def publish_result(redis_client, uuid, decoded_text):
+def publish_result(redis_client, decoded_text):
     try:
-        redis_client.publish('image-response-queue', f"{uuid}:{decoded_text}")
-        logging.debug(f"Published result to 'response-queue' for UUID: {uuid}")
+        redis_client.publish('image-response-queue', f"{thread_local_storage.uuid}:{decoded_text}")
+        logging.debug("Published result to 'response-queue'")
     except redis.exceptions.ConnectionError as e:
         logging.error(f"Redis Connection Error while publishing: {e}")
 
@@ -42,26 +46,30 @@ def handle_message(redis_client, message):
 
         message_data = message['data'].decode('utf-8')
         uuid, encoded_image = message_data.split(':', 1)
-        logging.info(f"Received message for UUID: {uuid}")
+
+        thread_local_storage.uuid = uuid
+
+        logging.info("Received message")
 
         upload_request = ImageUploadRequest(uuid=uuid, image=encoded_image)
         nparr = np.frombuffer(upload_request.decode_image(), np.uint8)
         preprocessed_img = preprocess_image(nparr)
         decoded_text = perform_ocr(preprocessed_img)
 
-        logging.info(f"OCR result for UUID: {uuid} - {decoded_text}")
+        logging.info(f"OCR result: {decoded_text}")
 
-        publish_result(redis_client, uuid, decoded_text)
+        publish_result(redis_client, decoded_text)
 
-        end_time = time.perf_counter()  # End high-resolution timer
+        end_time = time.perf_counter()
         processing_time = end_time - start_time
-        logging.info(f"Processed and published OCR result for UUID: {uuid} in {processing_time:.3f} seconds")
+        logging.info(f"Processed and published OCR result in {processing_time:.3f} seconds")
 
     except ValidationError as e:
-        logging.error(f"Validation Error for UUID: {uuid} - {e}")
+        logging.error(f"Validation Error: {e}")
     except Exception as e:
-        logging.error(f"Unhandled Exception for UUID: {uuid} - {e}")
-
+        logging.error(f"Unhandled Exception: {e}")
+    finally:
+        del thread_local_storage.uuid
 
 def create_redis_client():
     return redis.Redis(host='tenman.ee', port=6379, db=0, socket_timeout=5)
@@ -87,12 +95,22 @@ class MillisecondFormatter(logging.Formatter):
         ms = int(record.msecs)
         return f"{created}.{ms:03d}"
 
-def main():
-    logging_format = "%(asctime)s - %(levelname)s - %(message)s"
+class CustomLogRecord(logging.LogRecord):
+    def __init__(self, *args, **kwargs):
+        super(CustomLogRecord, self).__init__(*args, **kwargs)
+        uuid = getattr(thread_local_storage, 'uuid', None)
+        self.uuid = f"[UUID: {uuid}] " if uuid else ""
+
+def setup_logging():
+    logging.setLogRecordFactory(CustomLogRecord)
+    logging_format = "%(asctime)s - %(levelname)s - %(uuid)s%(message)s"
     formatter = MillisecondFormatter(logging_format)
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG, format=logging_format)
     for handler in logging.root.handlers:
         handler.setFormatter(formatter)
+
+def main():
+    setup_logging()
 
     redis_client = None
     timeout_seconds = 10
@@ -103,7 +121,7 @@ def main():
         try:
             if not redis_client or not check_redis_connection(redis_client):
                 redis_client = create_redis_client()
-                is_subscribed = False  # Reset subscription status
+                is_subscribed = False
 
             if not is_subscribed:
                 subscribe_to_queue(redis_client)
@@ -117,11 +135,11 @@ def main():
 
         except redis.exceptions.ConnectionError as e:
             logging.error(f"Redis Connection Error: {e}. Reconnecting in {timeout_seconds} seconds...")
-            redis_client = None  # Reset Redis client
+            redis_client = None
             time.sleep(timeout_seconds)
         except Exception as e:
             logging.error(f"Unhandled Exception: {e}. Reconnecting in {timeout_seconds} seconds...")
-            redis_client = None  # Reset Redis client
+            redis_client = None
             time.sleep(timeout_seconds)
 
         time.sleep(1)
